36cab9b33eeca9dd0d7580ced1c0af1d4a53c450
Ryan Guimarães <ryan.guimar10@gmail.com>
2020-10-23 01:30:41 -0300

    Adicionado funcionalidade de alternar entre feito e a fazer
    As notas agora são adicionadas como a fazer e podem ser marcadas e desmarcadas como feito

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 
 ## Objetivos:
  - [x] Estrutura HTML
- - [ ] Estilização
+ - [x] Estilização
  - [ ] Implementar troca de temas
  - [ ] Implementar botão de descida rápida
  - [x] Implementar adição de notas
- - [ ] Implementar edição de notas
+ - [x] Implementar edição de notas
  - [ ] Implementar exclusão de notas
  - [ ] Implementar pesquisa de notas
  - [ ] Implementar armazenamento das notas
@@ -18,3 +18,5 @@
  - O uso de variáveis vai facilitar a troca de temas que será futuramente implementada.
  - Font Aweasome pode ser utilizado em pseudo-elementos ::before e ::after
  - O uso do innerHTML para incluir a nota não funcionou corretamente, porém o insertAdjacentElement funcionou perfeitamente
+- O innerHTML funcionou, porém apenas quando o "placeholder de nota" se encontrava fora do .notes-container, porém por uma futura convenção eu decidi utilizar o insertAdjacentElement
+- Removido o botão de pesquisa da barra de procura
--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const addNoteButton = document.querySelector("#add-note")
 const noteContainer = document.querySelector(".notes-container")
-
+const togglePriority = (event) => {
+    const note = event.path[2]
+    const checkboxValue = event.target.checked
+    if(checkboxValue == true){
+        note.classList.remove('did')
+        note.classList.add('todo')
+        return
+    }
+    note.classList.remove('todo')
+    note.classList.add('did')
+}
 
 const note = `
-<div class="note did">
+<div class="note todo">
 <div class="row-buttons">
-    <input type="checkbox" class="toggle-priority" checked>
+    <input type="checkbox" onclick="togglePriority(event)" class="toggle-priority" checked>
     <input type="checkbox" class="mark-as-deletable">   
 </div>
 <span class="note-title" contenteditable="true"></span>
@@ -13,7 +23,7 @@ const note = `
 </div>
 `
 const addNote = () => {
-    noteContainer.insertAdjacentHTML('beforeEnd',note)
+    noteContainer.insertAdjacentHTML('afterBegin',note)
 }
 
 addNoteButton.addEventListener("click", addNote)
